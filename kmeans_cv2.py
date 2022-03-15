e843d80a6e9092e8ce3e83fed98420e1ba3b862b
tianjiao-j <jiangtianjiao1685@gmail.com>
2022-03-15 11:52:34 +1030

    use-hue-for-clustering

--- kmeans_cv2.py
+++ kmeans_cv2.py
@@ -4,24 +4,22 @@ from matplotlib import pyplot as plt
 import webcolors
 from datetime import datetime
 
-img = cv2.imread("images/face.jpeg")
+img = cv2.imread("images/trousers-black.png")
 
 
-def detect_colors(image, num_clusters, num_iters, resize_factor, crop_factor):
+def detect_colors(image, num_clusters, num_iters, resize_factor, crop_factor, type="hue"):
     height, width, depth = image.shape
     # crop
     crop_factor = (100 - crop_factor) / 2
     image = image[int(height * crop_factor / 100):(height - int(height * crop_factor / 100)),
             int(width * crop_factor / 100):(width - int(width * crop_factor / 100))]
     cv2.imwrite('output/cropped.jpg', image)
-    # cv2.imshow('output/cropped.jpg', image)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
 
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image_hsv = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+
     start_time = datetime.now()
 
-    # scale_percent = 30  # percent of original size
     w_resize = int(image.shape[1] * resize_factor / 100)  # vertical
     h_resize = int(image.shape[0] * resize_factor / 100)  # horizontal
     dim = (w_resize, h_resize)
@@ -32,46 +30,89 @@ def detect_colors(image, num_clusters, num_iters, resize_factor, crop_factor):
 
     Z = image_flat
     Z = np.float32(Z)
-    print(Z.shape)
-    # define criteria and apply kmeans()
-    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, num_iters, 1.0)
-    ret, label, center = cv2.kmeans(Z, num_clusters, None, criteria, 100, cv2.KMEANS_RANDOM_CENTERS)
 
-    for i in range(num_clusters):
-        color = webcolors.rgb_to_hex(center[i].astype(int))
-        samples = Z[label.ravel() == i]
+    if (type == "hue"):
+        # convert to HSV and use Hue value only
+        # resize image
+        image_hsv = cv2.resize(image_hsv, dim, interpolation=cv2.INTER_AREA)
+        w, h, d = image_hsv.shape
+        image_flat_hsv = np.reshape(image_hsv, (w * h, 3))
 
-        # mapping the clusters
-        for j in range(len(Z)):
-            if (label[j] == i):
-                Z[j] = center[i]
+        Z_hsv = image_flat_hsv
+        Z_hsv = np.float32(Z_hsv)
+        Z_hsv = Z_hsv[:, 0]
 
-        percentage = len(samples) * 100 / Z.shape[0]
-        percentage_str = str(i) + "-" + str(percentage) + "%"
-        print("cluster info", percentage_str)
-        # print(webcolors.hex_to_name(color))  # fixme: color name not found
-        plt.scatter(samples[:, 0], samples[:, 1], c=color)
-        plt.scatter(center[i, 0], center[i, 1], s=3000, c='black', marker=r"$ {} $".format(percentage_str))
-    print("centers", center)
-    delay = datetime.now() - start_time
-    print("delay", delay.total_seconds())
+        # define criteria and apply kmeans()
+        criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, num_iters, 1.0)
+        ret, label, center = cv2.kmeans(Z_hsv, num_clusters, None, criteria, 100, cv2.KMEANS_RANDOM_CENTERS)
 
-    # reconstruct image
-    Z = Z.reshape((w, h, 3))
-    # print(Z.shape)
-    cv2.imwrite('output/mapping.jpg', cv2.cvtColor(Z, cv2.COLOR_RGB2BGR))
-    # cv2.imshow("mapping", Z)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
-    plt.savefig('output/clusters.jpg')
-    plt.show()
+        # find centers by RGB values of cluster samples
+        for i in range(num_clusters):
+            samples = Z[label.ravel() == i]  # RGB values
+            center = np.mean(samples, axis=0)
+            color = webcolors.rgb_to_hex(center.astype(int))
 
-    # img1 = cv2.imread("output/cropped.jpg")
-    # img2 = cv2.imread("output/mapping.jpg")
-    # img3 = cv2.imread("output/clusters.jpg")
-    #
-    # imgh = np.vstack([img1, img2, img3])
-    # cv2.imshow("all", imgh)
-    # cv2.imwrite("output/all_fig.jpg", imgh)
+            # mapping the clusters
+            for j in range(len(Z)):
+                if (label[j] == i):
+                    Z[j] = center
 
-detect_colors(img, num_clusters=5, num_iters=50, resize_factor=50, crop_factor=50)
+            percentage = len(samples) * 100 / Z.shape[0]
+            percentage_str = str(i) + "-" + str(percentage) + "%"
+            print "########## cluster info", percentage_str
+            print center
+            # print(webcolors.hex_to_name(color))  # fixme: color name not found
+            plt.scatter(samples[:, 0], samples[:, 1], c=color, label=percentage_str, s=200)
+            # plt.scatter(center[0], center[1], s=3000, c='black', marker=r"$ {} $".format(percentage_str))
+            plt.legend(loc=2, prop={'size': 20})
+
+        # print("centers", center)
+        delay = datetime.now() - start_time
+        print "**************** delay", delay.total_seconds()
+
+        # reconstruct image
+        Z = Z.reshape((w, h, 3))
+        cv2.imwrite('output/mapping.jpg', cv2.cvtColor(Z, cv2.COLOR_RGB2BGR))
+        plt.savefig('output/clusters.jpg')
+        plt.show()
+
+    elif (type == "rgb"):
+        # define criteria and apply kmeans()
+        criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, num_iters, 1.0)
+        ret, label, center = cv2.kmeans(Z, num_clusters, None, criteria, 100, cv2.KMEANS_RANDOM_CENTERS)
+
+        for i in range(num_clusters):
+            color = webcolors.rgb_to_hex(center[i].astype(int))
+            samples = Z[label.ravel() == i]
+
+            # mapping the clusters
+            for j in range(len(Z)):
+                if (label[j] == i):
+                    Z[j] = center[i]
+
+            percentage = len(samples) * 100 / Z.shape[0]
+            percentage_str = str(i) + "-" + str(percentage) + "%"
+            print "########## cluster info", percentage_str
+            # print(webcolors.hex_to_name(color))  # fixme: color name not found
+            plt.scatter(samples[:, 0], samples[:, 1], c=color, label=percentage_str, s=200)
+            # plt.scatter(center[i, 0], center[i, 1], s=3000, c='black', marker=r"$ {} $".format(percentage_str))
+            plt.legend(loc=2, prop={'size': 20})
+
+        print "@@@@@@@@@ centers"
+        print center
+        delay = datetime.now() - start_time
+        print "**************** delay", delay.total_seconds()
+
+        # reconstruct image
+        Z = Z.reshape((w, h, 3))
+        cv2.imwrite('output/mapping.jpg', cv2.cvtColor(Z, cv2.COLOR_RGB2BGR))
+        plt.savefig('output/clusters.jpg')
+        plt.show()
+
+    else:
+        print "Error: type not defined. Type must be either hue or rgb."
+
+
+# def find_color_name(rgb, table):
+
+detect_colors(img, num_clusters=5, num_iters=50, resize_factor=10, crop_factor=100, type="rgb")
